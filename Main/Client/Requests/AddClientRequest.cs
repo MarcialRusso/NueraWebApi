f683243a067be671ebb92c7b8e4d2fccea9e6695
E90877 <mrusso@atb.com>
2023-07-26 15:23:23 -0600

    Added Client contract

---
+++ Main/Client/Requests/AddClientRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Main.Client.Requests
+{
+    /// <summary>
+    /// Represents a client creation request
+    /// </summary>
+    public class AddClientRequest
+    {
+        /// <summary>
+        /// The Client's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The Client's Email
+        /// </summary>
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The Client's CC
+        /// </summary>
+        [CreditCard]
+        public int CreditCard { get; set; }
+    }
+}
